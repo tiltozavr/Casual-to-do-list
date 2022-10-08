44e7dec186a505029a8f59048c973db1181dea5e
tiltozavr <ragimshin@gmail.com>
2022-10-08 20:45:41 +0300

    fix js file

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
-let addMessage = document.querySelector(`.form__input`)
-let addButton = document.querySelector(`.form__btn`);
-let container = document.querySelector(`.main-container`);
-let table = document.querySelector(`.main-container__table`);
+const addMessage = document.querySelector(`.form__input`)
+const addButton = document.querySelector(`.form__btn`);
+const container = document.querySelector(`.main-container`);
+const table = document.querySelector(`.main-container__table`);
+const ENDPOINT = "https://todolist-matveev.herokuapp.com/api/v1/"
+const URL = "https://api.binance.com/api/v3/"
+
 
 let toDoList = []
 
+
+
+async function getToDoLists(endpoint) {
+    const resp = await fetch(endpoint + "time", {
+        mode: 'no-cors',
+        method: 'GET',
+    });
+    const respData = await resp.json();
+    console.log(respData);
+}
+
+getToDoLists(URL)
 
 function formToDo() {
     let toDo = {
