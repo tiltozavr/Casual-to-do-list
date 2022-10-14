dcd0b73fce97001da143b2b1d8a459ddcab9bc92
tiltozavr <ragimshin@gmail.com>
2022-10-14 22:46:59 +0300

    добавил новую кнопку для добавления туду, скорректировал урл

--- main.js
+++ main.js
@@ -1,14 +1,10 @@
 const addMessage = document.querySelector(`.form__input`)
-const addButton = document.querySelector(`.form__btn`);
 const container = document.querySelector(`.table__container`);
 const table = document.querySelector(`.table`);
-const INPUT_ELEMENTS = document.querySelectorAll(`.table__btn`)
-const ADDNEW_BUTTON = document.querySelector(`.addnewbutton`);
 const ADDNEW_BUTTON_CONTAINER = document.getElementById('addnewbutton__container');
-const CREATE_FORM = document.createElement('form');
-const CREATE_INPUT = document.createElement('input');
-const CREATE_BUTTON = document.createElement('button');
-const ENDPOINT = "https://todolist-matveev.herokuapp.com/api/v1/"
+const NEW_FORM = document.getElementById(`newForm`);
+// const ENDPOINT = "https://todolist-matveev.herokuapp.com/api/v1/"
+const ENDPOINT = "https://tirawian-to-do-list-node.herokuapp.com/"
 
 function init() {
     getToDoLists(ENDPOINT)
@@ -17,7 +13,7 @@ function init() {
 init()
 
 async function getToDoLists(urlPath) {
-    const resp = await fetch(urlPath + "todolist", {
+    const resp = await fetch(urlPath + "tasks", {
         method: 'GET'
     });
     const respData = await resp.json();
@@ -25,18 +21,19 @@ async function getToDoLists(urlPath) {
 }
 
 async function createToDoList(urlPath, data) {
-    const resp = await fetch(urlPath + "createtodo", {
+    const resp = await fetch(urlPath + "tasks", {
         method: 'POST',
         body: JSON.stringify(data),
         headers: {
             "Content-type": "application/json"
         }
     });
+    const respData = await resp.json();
 }
 
-function formToDo() {
-    createToDoList(ENDPOINT, {"text": addMessage.value, "done": false, "priority": 1});
-    getToDoLists(ENDPOINT)
+async function formToDo() {
+    await createToDoList(ENDPOINT, {"text": addMessage.value, "done": false, "priority": 1});
+    await getToDoLists(ENDPOINT)
 };
 
 function createToDo(data) {
@@ -44,6 +41,11 @@ function createToDo(data) {
     for (let i = 0; i < data.length; i++) {
         let liElement = document.createElement(`li`)
         let inputElement = document.createElement(`input`)
+        let buttonEl = document.createElement(`button`);
+        buttonEl.type = "button"
+        buttonEl.onclick = function() {
+            deleteToDo(ENDPOINT, data[i].id)
+        }
         inputElement.type = "checkbox"
         if (data[i].done === true) {
             liElement.style.textDecoration = "line-through"
@@ -55,41 +57,38 @@ function createToDo(data) {
         liElement.type = "none"
         liElement.id = data[i].id
         inputElement.onclick = function() {
-            (changeIsDone(ENDPOINT, data[i].id, data[i].done, data[i].text))
+            data[i].done = data[i].done === true ? false : true; 
+            (changeToDo(ENDPOINT, data[i]))
         }
         liElement.textContent = `${data[i].text}`
         liElement.classList.add("table__item")
+        liElement.prepend(buttonEl)
         liElement.prepend(inputElement)
         table.append(liElement)
     }
 }
 
 async function deleteToDo(urlPath, id) {
-    const resp = await fetch(urlPath + "deletetodo?id=" + id, {
-        method: "GET"
+    const resp = await fetch(urlPath + "tasks/" + id, {
+        method: "DELETE"
     })
     getToDoLists(ENDPOINT)
 }
 
-async function changeIsDone(urlPath, id, isDone, text) {
-    if (isDone === true) {
-        const resp = await fetch(urlPath + "updatetodo?id=" + id + "&text=" + text + "&done=" + false)
-    } else {
-        const resp = await fetch(urlPath + "updatetodo?id=" + id + "&text=" + text + "&done=" + true)
-    }
+async function changeToDo(urlPath, data) {
+    const resp = await fetch(urlPath + "tasks/" + data.id, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            "Content-type": "application/json"
+        }
+    });
     getToDoLists(ENDPOINT) 
 }
 
 function createInputForm() {
-    ADDNEW_BUTTON.remove();
-    const CREATE_FORM = document.createElement('form');
-    CREATE_INPUT.classList.add('form__input');
-    CREATE_BUTTON.textContent = "Add";
-    CREATE_BUTTON.classList.add('addnewbutton');
-    CREATE_BUTTON.onclick = formToDo;
-    CREATE_BUTTON.type = "button";
-    CREATE_FORM.append(CREATE_INPUT, CREATE_BUTTON);   
-    ADDNEW_BUTTON_CONTAINER.append(CREATE_FORM)   
+    NEW_FORM.classList.toggle("addnewbutton-none");
+    ADDNEW_BUTTON_CONTAINER.classList.toggle("addnewbutton-none");
 }
 
 
